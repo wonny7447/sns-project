9d4dad184fdb10c1079009c26cfa09337d906e8a
wonny7447 <wonny7447@naver.com>
2021-05-07 22:21:02 +0900

    a

--- sns_project/main/views.py
+++ sns_project/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-
 def showmain(request):
     return render(request, 'main/mainpage.html')
 
-def show(request):
-    return render(request, 'main/show.html')
+def history(request):
+    return render(request, 'main/history.html')
+
+def like(request):
+    return render(request, 'main/like.html')
--- sns_project/sns_project/urls.py
+++ sns_project/sns_project/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.showmain),
-    path('show/', views.show),
+    path('', views.showmain, name="showmain"),
+    path('history/', views.history, name="history"),
+    path('like/', views.like, name="like"),
 ]
