74a215940bef1c2015b2d33a60bdb5c9bbda2bee
wonny7447 <wonny7447@naver.com>
2021-07-19 18:59:47 +0900

    210719 following 개발 과제

--- sns_project/users/views.py
+++ sns_project/users/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from main.models import Post
+from .models import User
+
 
 # Create your views here.
+def mypage(request, id):
+    user = get_object_or_404(User, pk=id)
+    context = {
+        'user' : user,
+        'qna' : Post.objects.filter(writer=user),
+        'followings':user.profile.followings.all(),
+        'followers':user.profile.followers.all(),
+    }
+    return render(request, 'users/mypage.html', context)
 
-def mypage(request):
-    user = request.user
-    qna = Post.objects.filter(writer=user)
-    return render(request, 'users/mypage.html', {'qna':qna})
+
+def follow(request,id):
+    user=request.user
+    followed_user=get_object_or_404(User,pk=id)
+    is_follower=user.profile in followed_user.profile.followers.all()
+    if is_follower:
+        user.profile.followings.remove(followed_user.profile)
+    else:
+        user.profile.followings.add(followed_user.profile)
+    return redirect('users:mypage', followed_user.id)
